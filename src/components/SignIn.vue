af0a8abc4b8f5563b6dfcb321d2c1f95661d9d58
me <coder@coder.com>
2023-06-24 13:12:19 +0600

    Workign on signup and signin pages component --> /Auth

---
+++ src/components/SignIn.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Sign In</h1>
+    <input type="text">
+    <input type="password">
+</template>
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,13 +1,32 @@
 <script>
+import { ref } from 'vue';
+import SignIn from '../components/SignIn.vue';
+import SignUp from '../components/SignUp.vue';
+
 export default {
-    mounted(){
+    setup() {
+        const authComponent = ref("signIn")
+        return {
+            authComponent
+        }
+    },
+    mounted() {
         console.log(this.$route)
+    },
+    components: {
+        SignIn, SignUp
     }
 }
 </script>
 
 <template>
     <section>
-        <h1>Authorization {{ this.$route.params.user }}</h1>
+        <button @click="authComponent = 'signIn'" class="btn"
+            :class="{ 'active': authComponent === 'signIn' }">Login</button>
+        <button @click="authComponent = 'signUp'" class="btn"
+            :class="{ 'active': authComponent == 'signUp' }">Signup</button>
+        <keep-alive>
+            <component :is="authComponent"></component>
+        </keep-alive>
     </section>
 </template>
