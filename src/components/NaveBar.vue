8f1d8dd68bc5bb8df4ae926d79453e688ddff04e
me <coder@coder.com>
2023-06-27 15:26:13 +0600

    Navebar Done

---
+++ src/components/NaveBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <div class="icon">
+            <img src="../assets/img/logo.png" alt="News Logo" srcset="../assets/img/logo.png">
+        </div>
+        <div class="nav-title">
+            <h2>News</h2>
+        </div>
+    </nav>
+    <div class="break">
+        <br><br><br><br><br><br>
+    </div>
+</template>
+<style scoped>
+
+</style>
