927003c2b270f612473401c875d6844dedf19f43
me <coder@coder.com>
2023-06-27 15:32:06 +0600

    Change on home pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,17 @@ import { ref } from "vue";
 import Comment from "../components/icons/Comment.vue";
 import LoveOutline from "../components/icons/LoveOutline.vue";
 import Share from "../components/icons/Share.vue";
+import NavBar from "../components/NaveBar.vue";
 
 const title = ref("New news application launched recently")
 const postLink = ref({
     link: "/news/post?show=New Application Released",
     img: "https://i0.wp.com/uxcrush.com/wp-content/uploads/2021/12/instagram-desktop-ui-in-figma.jpg"
 })
+
 </script>
 <template>
+    <NavBar />
     <section class="posts-container">
         <div v-for="i in 5" class="post center-X" :key="'string'+i">
             <RouterLink :to="postLink.link" class="post-title">
