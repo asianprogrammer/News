267852c44552148c8443f67d8ebe1a79eb854dc2
me <coder@coder.com>
2023-06-28 11:25:46 +0600

    🎉 Top Post

---
+++ src/components/TopPost.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <section>
+        <h4>Top POST</h4>
+        <div class="list flex">
+            <RouterLink to="/">New news application launched recently</RouterLink>
+            <RouterLink to="/">Warning! Are You Sabotaging Your Wealth?</RouterLink>
+            <RouterLink to="/">Let's Stop Feeling Broke</RouterLink>
+        </div>
+    </section>
+</template>
+<style scoped>
+.list {
+    padding: 15px 0;
+    margin: 10px 0;
+    flex-direction: column;
+}
+
+.list a {
+    margin: 5px 0;
+    max-width: 300px;
+    padding: 10px 20px;
+    overflow: hidden;
+    white-space: nowrap;
+    border-radius: 4px;
+    text-overflow: ellipsis;
+}
+
+.list a:hover {
+    background: var(--gray);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,49 @@
 <script setup>
 import NavBar from "../components/NaveBar.vue";
 import Post from "../components/Post.vue";
+import TopPost from "../components/TopPost.vue";
 </script>
 <template>
     <NavBar />
-    <Post />
+    <section class="flex align-item-center container">
+        <Post />
+        <TopPost class="top-post" />
+    </section>
 </template>
+<style scoped>
+.container {
+    width: 90%;
+    align-items: center;
+    justify-content: center;
+}
+
+.top-post {
+    top: 120px;
+    right: 50px;
+    z-index: 9;
+    padding: 15px 20px;
+    position: fixed;
+    border-radius: 4px;
+    background: #ffffff;
+    box-shadow: 4px 5px 13px 0px rgba(0, 0, 0, 0.05);
+}
+
+@media only screen and (max-width: 1170px) {
+    .container {
+        width: 80%;
+    }
+}
+
+@media only screen and (max-width: 800px) {
+    .top-post {
+        position: static;
+        align-items: normal;
+        flex-direction: column-reverse;
+    }
+
+    .container {
+        width: 100%;
+        flex-direction: column-reverse;
+    }
+}
+</style>
